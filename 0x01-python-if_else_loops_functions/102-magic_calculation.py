af6442c772e75b9832758db5f81c88c7b10bd34e
ZintleZ <sesethuz@gmail.com>
2022-11-30 14:52:40 +0000

    fix

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,8 +3,8 @@ import dis
 
 def magic_calculation(a, b, c):
     if a < b:
-       return c 
+        return c 
     elif c > b:
-       return a + b
+        return a + b
     else:
-       return a * b - c
+        return a * b - c
